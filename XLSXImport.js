05bd17fe32c48220e22cbbb4770d31eea8d5cc0e
Mohammed Almosawi <33073985+mohif1995@users.noreply.github.com>
2022-05-20 02:21:54 +0300

    Add files via upload

--- XLSXImport.js
+++ XLSXImport.js
@@ -1,42 +1,71 @@
-
 var XLSX = require("xlsx");
-
 const fs = require('fs-extra');
-const { toSystemPath } = require('../../../lib/core/path');
+const {
+    toSystemPath
+} = require('../../../lib/core/path');
 
-exports.XLSXImport = async function(options) {
+//made possible by https://sheetjs.com/
 
-	options = this.parse(options);
-	 let path = toSystemPath(this.parseRequired(this.parse(options.path), 'string', 'fs.exists: path is required.'));
-	    let fields = options.fields;
-        let header = options.header;
-		//let data = options.path;
-		const file = XLSX.readFile(path);
-	//	var ws = XLSX.readFile(path);
-	//	var ws = XLSX.read(path);
-    //	const workbook = XLSX.readFile(path);
-		//	console.log(workbook);
-      
-	//	var data =XLSX.utils.sheet_to_json(workbook);
-	    // Grab the sheet info from the file
-    const sheetNames = file.SheetNames;
-    const totalSheets = sheetNames.length;
-    // Variable to store our data 
+exports.XLSXImport = async function (options) {
+
+    options = this.parse(options);
+    let path = toSystemPath(this.parseRequired(this.parse(options.path), 'string', 'fs.exists: path is required.'));
+    let fields = options.fields;
+    let headers = options.header;
+    let allsheet = options.allsheet;
+    let file = XLSX.readFile(path);
+    let keys = fields.length;
+    let hdr = [];
+    // Variable to store our data
     let parsedData = [];
+
+    //if the all sheet option checked
+    if (allsheet) {
+        var sheetNames = file.SheetNames;
+        var totalSheets = sheetNames.length;
+        console.log(totalSheets);
+    } else {
+        var sheetNames = file.SheetNames;
+        var totalSheets = 1;
+
+    }
+
+    //if the header option checked
+    if (headers) {
+
+        hdr = false;
+
+    } else {
+        for (let i = 0; i < 1; i++) {
+
+            const testdata = XLSX.utils.sheet_to_json(file.Sheets[sheetNames[i]], {
+                header: 1
+            });
+            var headerStorage = testdata[0].length;
+
+            if (headerStorage > keys) {
+
+                throw new Error('Youre Defin col is messing colloms');
+            } else if (headerStorage < keys) {
+                throw new Error('Youre Defin col is more than required');
+            } else {
+
+                hdr.push(...fields);
+            }
+        }
+    }
     // Loop through sheets
     for (let i = 0; i < totalSheets; i++) {
         // Convert to json using xlsx
-	//مهم	const tempData = XLSX.utils.sheet_to_json(file.Sheets[sheetNames[i]]);
-        const tempData = XLSX.utils.sheet_to_json(file.Sheets[sheetNames[i]]);//,{ });
-        // Skip header row which is the colum names
-        //tempData.shift();
+
+        const tempData = XLSX.utils.sheet_to_json(file.Sheets[sheetNames[i]], {
+            header: hdr
+        });
+
         // Add the sheet's json to our data array
         parsedData.push(...tempData);
     }
-		  
-		
-		return parsedData;
-		
+
+    return parsedData;
+
 }
-		
-        
